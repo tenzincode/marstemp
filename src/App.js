43f6064b46e8867d92ba9527e6ccd7035f4a232a
citizenofearth <namgyal@gmail.com>
2020-03-07 01:07:15 -0500

    Connect Axios to NASA Insight

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+const API_URL = 'https://api.nasa.gov/insight_weather/?api_key=REMOVED_API_KEY&feedtype=json&ver=1.0'
+
+class App extends Component {
+  state = {
+    marstemp: []
+  }
+  componentDidMount() {
+    const url = `${API_URL}`;
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({ marstemp: data })
+      console.log(this.state.marstemp)
+    })
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-8">
+          <h1>React Axios Example</h1>
+          {JSON.stringify(this.state.marstemp)}
+        </div>
+       </div>
+    )
+  }
 }
 
 export default App;
