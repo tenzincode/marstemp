e49b6e8442a23ed51f4522bf0103d43296d6eb82
citizenofearth <namgyal@gmail.com>
2020-03-09 23:53:25 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,81 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-import axios from 'axios';
+import axios from 'axios'
 const API_URL = 'https://api.nasa.gov/insight_weather/?api_key=xem3ovPgeh9zWrw3fOyY4tTldhimR3ckKqnk9UoC&feedtype=json&ver=1.0'
 
 class App extends Component {
   state = {
-    marstemp: []
+    marsdata: [],
+    sols: [],
+    dates: [],
+    temps: [],
+    isLoading: true,
+    errors: null
   }
+
   componentDidMount() {
-    const url = `${API_URL}`;
+    const url = `${API_URL}`
     axios.get(url).then(response => response.data)
     .then((data) => {
-      this.setState({ marstemp: data })
-      console.log(this.state.marstemp)
+      let filteredTemps = []
+      let filteredDates = []
+
+      // JSON filter temperatures
+      for (var x in data) {
+        if (data['sol_keys'].indexOf(x) > -1) {
+          filteredTemps.push(data[x]['AT']['av'])
+        }
+      }
+
+      // JSON filter earth dates
+      for (var y in data) {
+        if (data['sol_keys'].indexOf(y) > -1) {
+          filteredDates.push(data[y]['First_UTC'])
+        }
+      }
+
+      this.setState({
+        marsdata: data,
+        sols: data['sol_keys'],
+        temps: filteredTemps,
+        dates: filteredDates,
+        isLoading: false
+      })
+      //console.log(this.state.marsdata)
     })
+
+    .catch(error => this.setState({ error, isLoading: false }))
   }
   
   render() {
+    const marsdata = this.state.marsdata
+    const sols = this.state.sols
+    const temps = this.state.temps
+    const dates = this.state.dates
+    const isLoading = this.state.isLoading
+    
+    console.log('sols:')
+    console.log(sols)
+    console.log('temps:')
+    console.log(temps)
+    console.log('dates:')
+    console.log(dates)
+    
     return (
-      <div className="container">
-        <div className="col-xs-8">
-          <h1>React Axios Example</h1>
-          {JSON.stringify(this.state.marstemp)}
+      <React.Fragment>
+        <div>
+          {!isLoading ? (
+            <div>
+              <h1>Mars Temps</h1>
+              
+              {temps}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
-       </div>
+      </React.Fragment>
     )
   }
 }
