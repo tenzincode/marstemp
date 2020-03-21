06792645d41cf85d13de3d184ffc8c38e18e3fe5
citizenofearth <namgyal@gmail.com>
2020-03-21 15:16:50 -0400

    Update Readme and some styles

--- src/App.js
+++ src/App.js
@@ -127,32 +127,71 @@ class App extends Component {
         </nav>
         {/* END NAV */}
 
+        {/* START HERO */}
+        <section className="hero is-medium is-bold">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h2 className=" has-text-white title">Latest Weather at Elysium Planitia</h2>
+              <p className=" has-text-white subtitle">InSight is taking daily weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’ equator.</p>
+            </div>
+          </div>
+        </section>
+        {/* END HERO */}
+
+        {/* START MAIN */}
         <div className="container">
-          <section className="articles row columns">
-            <div className="column is-8 is-offset-2">
+          {/* START WEATHER */}
+          <section className="articles">
+            <div className="row columns">
               {!isLoading ? (
                 marsdata.map(marstemp => {
                   const { sol, date, temp } = marstemp
                   return (
-                    /* START ARTICLE */
-                    <div className="card article" key={sol}>
-                      <div className="card-content" key={sol}>
-                        <div className="sol" key={sol}>
-                          <h2>Sol: {sol}</h2>
-                          <p>Earth date: {moment(date).format('LLLL')}</p>
-                          <p>Temperature: {temp} °Fahrenheit</p>
+                    <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd" key={sol}>
+                      <div className="card article" key={sol}>
+                        <div className="card-content" key={sol}>
+                          <div className="sol" key={sol}>
+                            <h3 className="title is-5 has-text-white">Sol: {sol}</h3>
+                            <p>Earth date: {moment(date).format('LLLL')}</p>
+                            <p>Temperature: {temp} °Fahrenheit</p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                    /* END ARTICLE */
                   )
                 })
-              ) : (
-                <p>Loading...</p>
-              )}
+                ) : (
+                  <p>Loading...</p>
+                  )}
+            </div>
+          </section>
+          {/* END WEATHER */}
+
+          <section>
+            <div className="container">
+              <div className="card">
+                <div className="card-content">
+                  <h2 className=" has-text-white title">InSight — Studying the 'Inner Space' of Mars</h2>
+                  <p className=" has-text-white subtitle">InSight, short for Interior Exploration using Seismic Investigations, Geodesy and Heat Transport, is a Mars lander designed to give the Red Planet its first thorough checkup since it formed 4.5 billion years ago. It is the first outer space robotic explorer to study in-depth the "inner space" of Mars: its crust, mantle, and core.</p>
+                  <p className=" has-text-white subtitle">Studying Mars' interior structure answers key questions about the early formation of rocky planets in our inner solar system - Mercury, Venus, Earth, and Mars - more than 4 billion years ago, as well as rocky exoplanets. InSight also measures tectonic activity and meteorite impacts on Mars today.</p>
+                  <p className=" has-text-white subtitle">The lander uses cutting edge instruments, to delve deep beneath the surface and seek the fingerprints of the processes that formed the terrestrial planets. It does so by measuring the planet's "vital signs": its "pulse" (seismology), "temperature" (heat flow), and "reflexes" (precision tracking).</p>
+                  <p className=" has-text-white subtitle">This mission is part of NASA's Discovery Program for highly focused science missions that ask critical questions in solar system science.</p>
+                </div>
+              </div>
             </div>
           </section>
         </div>
+        {/* END MAIN */}
+
+        {/* START FOOTER */}
+        <footer className="footer">
+          <div className="content has-text-centered">
+            <p>
+              <strong>Marštemp</strong> by <a href="https://tenzin.ca">Tenzin Namgyal</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+          </div>
+        </footer>
+        {/* END FOOTER */}
       </React.Fragment>
     )
   }
